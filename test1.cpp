16b95174db7ec2fb8dedae00e1524fd7b8afd80e
Wendell Bautista <wendellbautista@Wendells-MBP.hitronhub.home>
2018-04-27 18:59:31 -0700

    added a message to test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -7,4 +7,6 @@ int main(){
 	sum = 22 + 22;
 	cout << sum << endl;
 	cout << "this is pretty fricken awesome lol" << endl;
+
+	cout << "this is the change to master that branch 1 doesnt know about" << endl;
 }
