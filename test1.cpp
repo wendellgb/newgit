55ad72768b11646e7ba4590dfdc0e0d1aca10006
Wendell Bautista <wendellbautista@Wendells-MBP.hitronhub.home>
2018-04-27 19:10:33 -0700

    added some lines to test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -7,8 +7,6 @@ int main(){
 	sum = 22 + 22;
 	cout << sum << endl;
 	cout << "this is pretty fricken awesome lol" << endl;
-
-<<<<<<< HEAD
 	cout << "this line is not in master branch" << endl;
 	cout << "this is the change to master that branch 1 doesnt know about" << endl;
 
